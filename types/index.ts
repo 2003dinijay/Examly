a91cf8305ced179709c937111a87d977223c958e
Dinithi Nanayakkara <dinijayo@gmail.com>
2025-10-21 00:15:28 +0530

    refact:change types

--- types/index.ts
+++ types/index.ts
@@ -1,28 +1,29 @@
 import { serverTimestamp } from 'firebase/database';
 
 export interface AnswerOption {
-  key: number;
-  text: string;
+ key: number;
+ text: string;
 }
 
 export interface QuizQuestion {
-  id?: string;
-  question: string;
-  answers: AnswerOption[];
-  rightAnswer: number;
-  timestamp: ReturnType<typeof serverTimestamp>;
+ id?: string;
+ question: string;
+ answers: AnswerOption[];
+ rightAnswer: number;
+ timestamp: ReturnType<typeof serverTimestamp>;
 }
 
+
 export interface UserAnswer {
-  userEmail: string;
-  questionId: string;
-  answerId: number;
-  timestamp: ReturnType<typeof serverTimestamp>;
+ userName: string; 
+ questionId: string;
+ answerId: number;
+ timestamp: ReturnType<typeof serverTimestamp>;
 }
 
 
 export interface UserScore {
-  userEmail: string;
-  score: number;
-  lastAnswerTimestamp: number; 
+ userId: string; 
+ score: number;
+ lastAnswerTimestamp: number; 
 }
